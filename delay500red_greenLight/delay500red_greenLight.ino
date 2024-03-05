c8bf71a7f8c0a2ff42324694ff7d7f3f7a1c1ff2
Amadeus Alerta <efe3365@gmail.com>
2024-03-05 19:21:11 +0300

    Delay 500
    Delay 500 Red and Green Light Loop

---
+++ delay500red_greenLight/delay500red_greenLight.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(10, OUTPUT);
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+  delay (500);
+  digitalWrite(10,LOW);
+  digitalWrite(9,HIGH);
+  delay(500);
+}
